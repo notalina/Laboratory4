92ca8528050d472aa04732875302c8c320880942
notalina <60419199+notalina@users.noreply.github.com>
2020-10-24 23:43:02 +0500

    methods changed

--- data_access_layer.py
+++ data_access_layer.py
@@ -2,35 +2,38 @@ import pymysql
 
 class dataAccessLayer:
     #db = pymysql.connect("localhost", "flask_user", "flask_user", "flask_user", port=3306)
-    def select_execute(self, columns, table, condition):
+    def select_sql(self, columns, table, condition):
         query = "SELECT "
         columns_names = ""
         if (len(columns) == 0):
             columns = "*"
         else:
             columns_names = ",".join(columns)
-        query = f"{query} {columns_names} FROM {table} WHERE {condition} ;"
-        print(query)
+        return f"{query} {columns_names} FROM {table} WHERE {condition} ;"
 
-    def delete_execute(self, database, condition):
-        query = f"DELETE FROM {database} WHERE {condition} ;"
-        print(query)
+    def delete_sql(self, database, condition):
+        return f"DELETE FROM {database} WHERE {condition} ;"
 
-    def insert_execute(self, table_name, columns, values):
+    def insert_sql(self, table_name, columns, values):
         columns_names = ",".join(columns)
         values_str = ",".join(values)
-        query = f"INSERT {table_name}({columns_names}) VALUES ( {values_str} );"
-        print(query)
+        return f"INSERT {table_name}({columns_names}) VALUES ( {values_str} );"
 
-    def update()
+    def update_sql(table_name: str, update_data: Dict, where = "1=1"):
+        update_str_list = [f"{column}={value}" for column, value in update_data.items()]
+        update_expression = ", ".join(update_expression)
+        return f"UPDATE {table_name} SET {update_expression} WHERE {where};"
     
-    def drop_execute(self, table):
-        query = f"DROP TABLE  {table};"
-        print(query)
+    def drop_sql(self, table):
+        return f"DROP TABLE  {table};"
     
-    def alter_execute(self, table, ):
+    def alter_execute(self, table, condition):
+        return f"ALTER TABLE {table} {condition}"
 
-    def create_execute(self, ):
+    def create_sql(table_name: str, column_definitions: dict):
+        update_str_list = [f"{column_name} {column_type}" for column_name,column_type in column_definitions.items()]
+        update_expression = ", ".join(update_expression)
+        return f"CREATE TABLE {table_name} ({update_expression});"
 
 
     
